4e14b66c5c06c58ac90863c0407a6725791f3bef
Jared Wyce <wycejared@gmail.com>
2022-10-13 20:16:53 -0400

    💫 animation fun

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use figlet_rs::FIGfont;
 use rand::Rng;
-use std::collections::HashMap;
-use std::env;
 use std::fs;
-use std::io::Write;
+extern crate colorful;
+use std::{thread, time};
+
+use colorful::{Colorful, HSL};
 
 fn read_file_to_vector(path: &str) -> Vec<String> {
     return fs::read_to_string(path)
@@ -14,41 +15,28 @@ fn read_file_to_vector(path: &str) -> Vec<String> {
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let languages = read_file_to_vector("languages.txt");
-    let people = read_file_to_vector("people.txt");
-
-    let args: Vec<String> = env::args().collect();
-    let config = &args[1];
-
-    let mut results: HashMap<String, String> = HashMap::new();
-    match config as &str {
-        "all" => {
-            for person in &people {
-                let num_langs = languages.len();
-                results.insert(
-                    person.clone(),
-                    languages[rand::thread_rng().gen_range(0..num_langs)].clone(),
-                );
-            }
-        }
-        _ => {
-            let num_langs = languages.len();
-            let person = people
-                .iter()
-                .find(|&x| *x == config.to_string())
-                .expect("person not found");
-            results.insert(
-                person.clone(),
-                languages[rand::thread_rng().gen_range(0..num_langs)].clone(),
-            );
-        }
-    }
+    let num_langs = languages.len();
 
     let standard_font = FIGfont::standand().unwrap();
-    for (person, language) in &results {
-        let figure = standard_font
-            .convert(&format!("{person}:   {language}"))
-            .unwrap();
-        write!(&mut rainbowcoat::stdout(), "{}", figure).unwrap();
+    let rand_lang = languages[rng.gen_range(0..num_langs)].clone();
+
+    let one_second = time::Duration::from_secs(1);
+    let rand_secs = rng.gen_range(3..60);
+    for n in (1..=rand_secs).rev() {
+        print!("{esc}c", esc = 27 as char);
+        let fig_num = standard_font.convert(&format!("{n}")).unwrap();
+        println!("{}", fig_num.to_string());
+        thread::sleep(one_second);
     }
+
+    print!("{esc}c", esc = 27 as char);
+    let figure = standard_font.convert(&format!("{rand_lang}")).unwrap();
+    let text = format!("{:^50}", figure.to_string());
+    text.clone().rainbow_with_speed(4);
+
+    let p: f32 = rng.gen();
+    let h = (p * 1500.0 % 360.0) / 360.0;
+    println!("{}", text.gradient(HSL::new(h, 1.0, 0.5)))
 }
